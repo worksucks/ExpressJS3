3003382ed7be7abebced8dc79fb0a60d3b5da9d9
Tomasz Reizer <t_reizer@yahoo.com>
2019-07-21 16:07:17 +0200

    added server.js

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ var express = require('express');
 var app = express();
 app.use(express.static('assets'));
 
+app.use(function(req, res, next){
+  console.log('Hej, jestem pośrednikiem między żądaniem a odpowiedzią!');
+  next();
+});
+
 app.get('/', function(req,res){
   res.send('Hello World');
 })
 
 app.get('/store', function(req,res){
   res.send('To jest sklep');
-});
-
-app.use(function(req, res, next){
-  console.log('Hej, jestem pośrednikiem między żądaniem a odpowiedzią!');
-  next();
 });
 
 app.listen(3000);
